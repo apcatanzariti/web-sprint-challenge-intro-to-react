3df7be888905e68d1a2612581110562e56f245de
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-01-22 12:01:48 -0500

    working on MVP, just working on character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
 // Write your Character component here
+
+export default function Character ( {name, gender, location, species, id} ) {
+    return (
+        <div>
+            <h2>Name: {name}</h2>
+            <p>Gender: {gender}</p>
+            <p>Species: {species}</p>
+            <p>Location: {location}</p>
+        </div>
+    );
+}
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://rickandmortyapi.com/api/'
+export const API_KEY = 'character/1,2,3,4,5,6'
